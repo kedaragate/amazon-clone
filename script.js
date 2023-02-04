489295629ac41e151de39906f0290af962ac338b
Kedar Agate <kedar.agate@gmail.com>
2023-02-04 18:43:00 +0530

    landing-page

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ images.forEach((img, index) => {
   img.style.left = `${100 * index}%`;
 });
 
+window.onload = setInterval(nextSlider, 10000);
+
 function slider(transition) {
   images.forEach((img) => {
     img.style.transition = transition;
     img.style.transform = `translateX(${-counter * 100}%)`;
   });
 }
-const transition = "all 3s ease-in-out";
-const noTransition = "none";
-sliderNextBtn.addEventListener("click", () => {
+function nextSlider() {
   if (counter >= images.length - 1) {
     counter = 0;
     slider(noTransition);
@@ -30,9 +30,8 @@ sliderNextBtn.addEventListener("click", () => {
     counter++;
     slider(transition);
   }
-});
-
-sliderPrevBtn.addEventListener("click", () => {
+}
+function prevSlider() {
   if (counter <= 0) {
     counter = images.length - 1;
 
@@ -40,5 +39,24 @@ sliderPrevBtn.addEventListener("click", () => {
   } else {
     counter--;
     slider(transition);
+  }
+}
+const transition = "all 3s ease-in-out";
+const noTransition = "none";
+sliderNextBtn.addEventListener("click", nextSlider);
+
+sliderPrevBtn.addEventListener("click", prevSlider);
+
+const searchField = document.querySelector("#search-bar");
+const darkBackground = document.getElementsByClassName("dark-background")[0];
+const body = document.getElementsByTagName("body")[0];
+
+body.addEventListener("click", (e) => {
+  if (e.target !== searchField) {
+    darkBackground.classList.remove("dark-on-focus");
+  } else if (darkBackground.classList.contains("dark-on-focus")) {
+    darkBackground.classList.remove("dark-on-focus");
+  } else {
+    darkBackground.classList.add("dark-on-focus");
   }
 });
